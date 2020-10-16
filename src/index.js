0b11225453bae467372e9820abf5dbba786c4a8f
Kunal Kumar <kunalp78@gmail.com>
2020-10-17 03:29:16 +0530

    introduced chat app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
 
 const port  = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname,'../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port,()=>{
+io.on('connection',()=>{
+    console.log('New WEB Connection')
+})
+
+server.listen(port,()=>{
     console.log(`server is up and running${port}`)
 })
